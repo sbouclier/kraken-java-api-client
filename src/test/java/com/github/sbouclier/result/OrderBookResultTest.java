18de7b6519139d769986b7ee1f6c621109f9cf01
Stéphane Bouclier <sbouclier@users.noreply.github.com>
2017-07-31 21:38:57 +0200

    test market constructor

--- src/test/java/com/github/sbouclier/result/OrderBookResultTest.java
+++ src/test/java/com/github/sbouclier/result/OrderBookResultTest.java
@@ -5,8 +5,10 @@ import com.github.sbouclier.utils.StreamUtils;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.StringStartsWith.startsWith;
 
 /**
@@ -15,6 +17,15 @@ import static org.hamcrest.core.StringStartsWith.startsWith;
  * @author Stéphane Bouclier
  */
 public class OrderBookResultTest {
+
+    @Test
+    public void should_construct_market() {
+        OrderBookResult.Market market = new OrderBookResult.Market(BigDecimal.TEN, BigDecimal.ONE, 5000);
+
+        assertThat(market.price, equalTo(BigDecimal.TEN));
+        assertThat(market.volume, equalTo(BigDecimal.ONE));
+        assertThat(market.timestamp, equalTo(5000));
+    }
 
     @Test
     public void should_return_to_string() throws IOException {
